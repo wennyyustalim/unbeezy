1861b7231381ddd5891b6a42172f44bb674c26bc
Kenneth Halim <kanisiuskenneth@gmail.com>
2018-02-12 20:57:27 +0700

    add new activity to insert schedule, add schedule mockup layout

---
+++ app/src/main/java/com/sams/unbeezy/InsertScheduleActivity.java
@@ -0,0 +1,13 @@
+package com.sams.unbeezy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InsertScheduleActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_insert_schedule);
+    }
+}
--- app/src/main/java/com/sams/unbeezy/fragments/ScheduleFragment.java
+++ app/src/main/java/com/sams/unbeezy/fragments/ScheduleFragment.java
@@ -1,19 +1,38 @@
 package com.sams.unbeezy.fragments;
 
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.view.ContextMenu;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
 
+import com.google.gson.Gson;
+import com.sams.unbeezy.InsertScheduleActivity;
+import com.sams.unbeezy.MainActivity;
 import com.sams.unbeezy.R;
+
+import java.util.Random;
+
 
 /**
  * Created by kennethhalim on 2/12/18.
  */
 
 public class ScheduleFragment extends Fragment {
+    private String scheduleData = "[[{],[],[],[],[],[],[],]";
     @Override
     public View onCreateView(LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -21,6 +40,49 @@ public class ScheduleFragment extends Fragment {
         // properly.
         View rootView = inflater.inflate(
                 R.layout.fragment_schedule, container, false);
+        FloatingActionButton scheduleFAB = rootView.findViewById(R.id.schedule_insert_fab);
+        scheduleFAB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(),InsertScheduleActivity.class);
+                startActivity(intent);
+            }
+        });
+
         return rootView;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        Context context = getContext();
+        TableLayout tableLayout = view.findViewById(R.id.table_schedule);
+        for (int i =0; i<11;i++) {
+            TableRow tableRow = new TableRow(context);
+            tableRow.setWeightSum(6);
+            TextView textview = new TextView(context);
+            textview.setLayoutParams(new TableRow.LayoutParams(0, TableRow.LayoutParams.MATCH_PARENT, 1f));
+            textview.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+            textview.setBackground(getResources().getDrawable(R.drawable.border_right_primary));
+            textview.setText(String.format("%02d.00", i+7));
+            textview.setHeight(60);
+            textview.setGravity(Gravity.CENTER_VERTICAL | Gravity.CENTER_HORIZONTAL);
+            tableRow.addView(textview);
+
+            for(int j=1;j<=7;j++) {
+                TextView textViewColumn = new TextView(context);
+                textViewColumn.setLayoutParams(new TableRow.LayoutParams(0, TableRow.LayoutParams.MATCH_PARENT, 1f));
+                textViewColumn.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+                Random random = new Random();
+                textViewColumn.setBackgroundColor(random.nextInt()%256000);
+                textViewColumn.setText(String.format("%d%d", j,i+1));
+                textViewColumn.setHeight(80);
+                textViewColumn.setGravity(Gravity.CENTER_VERTICAL | Gravity.CENTER_HORIZONTAL);
+                tableRow.addView(textViewColumn);
+                textview.setPadding(1,1,1,1);
+            }
+            tableLayout.addView(tableRow);
+        }
+
     }
 }
