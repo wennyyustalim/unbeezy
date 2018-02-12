c835141dcc260b730773d39b2171836f6b2f3f69
Kenneth Halim <kanisiuskenneth@gmail.com>
2018-02-12 22:38:28 +0700

    add toolbars layout for activities other than main and login

--- app/src/main/java/com/sams/unbeezy/InsertScheduleActivity.java
+++ app/src/main/java/com/sams/unbeezy/InsertScheduleActivity.java
@@ -1,13 +1,19 @@
 package com.sams.unbeezy;
 
+import android.media.Image;
+import android.support.design.widget.AppBarLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toolbar;
 
 public class InsertScheduleActivity extends BaseActivity {
-
+    String ACTIVITY_TITLE ="Add New Schedule";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_insert_schedule);
+        setToolbar(ACTIVITY_TITLE);
     }
 }
