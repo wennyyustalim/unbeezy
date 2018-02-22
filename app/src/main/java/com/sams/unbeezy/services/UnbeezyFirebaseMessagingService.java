40831b036f5845f569d7bdc0dcb407f2fc4fb03f
reeechart <ferdinandusrichard@yahoo.co.id>
2018-02-22 21:44:56 +0700

    add message listener

--- app/src/main/java/com/sams/unbeezy/services/UnbeezyFirebaseMessagingService.java
+++ app/src/main/java/com/sams/unbeezy/services/UnbeezyFirebaseMessagingService.java
@@ -1,11 +1,65 @@
 package com.sams.unbeezy.services;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+
 import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+import com.sams.unbeezy.MainActivity;
+import com.sams.unbeezy.R;
 
 /**
  * Created by kennethhalim on 2/22/18.
  */
 
 public class UnbeezyFirebaseMessagingService extends FirebaseMessagingService {
+    private final int NOTIFICATION_ID = 3;
 
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+        Log.d("NOTIF","NOTIF RECEIVED");
+        if (remoteMessage.getNotification() != null) {
+            String notificationTitle = remoteMessage.getNotification().getTitle();
+            String notificationBody = remoteMessage.getNotification().getBody();
+            createNotification(notificationTitle, notificationBody);
+        }
+    }
+
+    private void createNotification(String title, String body) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, NOTIFICATION_ID, intent,
+                PendingIntent.FLAG_ONE_SHOT);
+
+        Uri defaultSoundUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationCompat.Builder notificationBuilder =
+                new NotificationCompat.Builder(this)
+                        .setContentTitle(title)
+                        .setContentText(body)
+                        .setAutoCancel(true)
+                        .setSound(defaultSoundUri)
+                        .setContentIntent(pendingIntent);
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        // Since android Oreo notification channel is needed.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel("channel",
+                    "Channel human readable title",
+                    NotificationManager.IMPORTANCE_DEFAULT);
+            notificationManager.createNotificationChannel(channel);
+        }
+
+        notificationManager.notify(NOTIFICATION_ID, notificationBuilder.build());
+    }
 }
