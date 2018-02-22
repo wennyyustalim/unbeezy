78c94e6c0ae7d72c8eb4f5bf7bb887a45c80eb17
Wenny Yustalim <wennyyustalim@gmail.com>
2018-02-22 19:57:12 +0700

    Get and display name initial on toolbar and user fragment

--- app/src/main/java/com/sams/unbeezy/BaseActivity.java
+++ app/src/main/java/com/sams/unbeezy/BaseActivity.java
@@ -1,11 +1,19 @@
 package com.sams.unbeezy;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.iid.FirebaseInstanceId;
@@ -19,7 +27,17 @@ import com.sams.unbeezy.services.FirebaseDatabaseService;
  */
 
 public abstract class BaseActivity extends AppCompatActivity{
+    String personName;
+    private String LOG_TAG = "BaseActivity";
+    SharedPreferences pref;
     Gson gson = new Gson();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        pref = getSharedPreferences("UnbeezyPref", Context.MODE_PRIVATE);
+    }
+
     @Override
     public void onStart(){
         super.onStart();
@@ -29,13 +47,20 @@ public abstract class BaseActivity extends AppCompatActivity{
 
     public void checkAuthenticated() {
         FirebaseAuth mAuth = FirebaseAuth.getInstance();
-
         FirebaseUser user = mAuth.getCurrentUser();
+
         if(user == null) {
             Intent intent = new Intent(this,LoginActivity.class);
             intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
             startActivity(intent);
             finish();
+        } else {
+            // Get the name of the person and store in shared preferences
+            personName = user.getDisplayName();
+            Log.d("BaseActivity", personName);
+            SharedPreferences.Editor editor = pref.edit();
+            editor.putString("personName", personName);
+            editor.apply();
         }
     }
     public void signOut() {
@@ -48,6 +73,7 @@ public abstract class BaseActivity extends AppCompatActivity{
     }
 
     public void setToolbar(String title) {
+
         ImageButton backButton = findViewById(R.id.toolbar_back_button);
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -57,6 +83,17 @@ public abstract class BaseActivity extends AppCompatActivity{
         });
         TextView titleView = findViewById(R.id.title_view);
         titleView.setText(title);
+        try {
+            personName = pref.getString("personName", null);
+            Log.d(LOG_TAG, String.format("Person's name: %s", personName));
+            String initial = Character.toString(personName.charAt(0));
+            Log.d(LOG_TAG, String.format("Initial: %s", initial));
+            TextView nameInitial = findViewById(R.id.name_initial);
+            nameInitial.setText(initial);
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+            Log.d(LOG_TAG, "Person's Name not found");
+        }
     }
 
 }
--- app/src/main/java/com/sams/unbeezy/fragments/UserFragment.java
+++ app/src/main/java/com/sams/unbeezy/fragments/UserFragment.java
@@ -1,13 +1,17 @@
 package com.sams.unbeezy.fragments;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.sams.unbeezy.PanicAttackActivity;
 import com.sams.unbeezy.R;
@@ -18,12 +22,16 @@ import com.sams.unbeezy.lists.DismisserServicesList;
  */
 
 public class UserFragment extends Fragment {
+    private String LOG_TAG = "UserFragment";
+    TextView personName;
+    String personNameStr;
+    SharedPreferences pref;
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setRetainInstance(true);
+        pref = this.getActivity().getSharedPreferences("UnbeezyPref", Context.MODE_PRIVATE);
     }
-
 
     @Override
     public View onCreateView(final LayoutInflater inflater,
@@ -41,6 +49,15 @@ public class UserFragment extends Fragment {
                 startActivity(intent);
             }
         });
+        personName = (TextView) rootView.findViewById(R.id.person_name);
+        try {
+            personNameStr = pref.getString("personName", null);
+            personName.setText(personNameStr);
+        } catch (NullPointerException e) {
+            Log.d(LOG_TAG, "Person's Name not found");
+
+        }
+
         return rootView;
     }
 }
