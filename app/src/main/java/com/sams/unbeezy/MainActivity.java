db17f47211c29db02958a19b82511fc9e290d775
Kenneth Halim <kanisiuskenneth@gmail.com>
2018-02-23 21:11:36 +0700

    course can be deleted by long press

--- app/src/main/java/com/sams/unbeezy/MainActivity.java
+++ app/src/main/java/com/sams/unbeezy/MainActivity.java
@@ -9,6 +9,7 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.ContextMenu;
 import android.view.View;
 import android.widget.Button;
 
@@ -102,4 +103,5 @@ public class MainActivity extends BaseActivity {
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
     }
+
 }
